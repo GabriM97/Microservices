14152e120d708e71dd0cf03533f0840c16e5e874
Gabriele <marino.gabri97@gmail.com>
2023-04-13 22:00:27 +0100

    Small refactoring to ServiceA's controller

--- ServiceA/src/Controller/ServiceController.php
+++ ServiceA/src/Controller/ServiceController.php
@@ -7,11 +7,14 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use GuzzleHttp\ClientInterface;
 use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7\Response;
 use Psr\Log\LoggerInterface;
-use Throwable;
 
 class ServiceController extends AbstractController
 {
+    /**
+     * @var array $payloads
+     */
     private array $payloads = [];
 
     private const SERVICE_B_ENDPOINT = 'http://service-b-api/process';
@@ -24,31 +27,23 @@ class ServiceController extends AbstractController
                 'data' => 'my_data',
             ],
             'Y' => [
-                'name' => 'Z', // 'Z' will make the validation fail in service-b
+                'name' => 'Z', // 'Z' will make the validation fail in ServiceB
                 'data' => 'my_data', // remove the 'data' field to get another validation error
             ],
         ];
     }
 
     /**
-     * Run the service with the passed {payload} which will need to match the route requirement.
+     * Run the service with the passed {payload} which will need to match the route requirement 'X' or 'Y'.
      *
      * @param  string $payload  the payload to use.
+     * 
      * @return JsonResponse
      */
     #[Route("/service/run/{payload}", name: "service_run", methods: 'GET', requirements: ['payload' => '[X|Y]{1}'])]    
     public function run(string $payload) : JsonResponse 
     {
-        try {
-            $this->logger->info('Sending payload "' . $payload . '" to "' . self::SERVICE_B_ENDPOINT .'"');
-
-            // fire http request to ServiceB using one of the payloads.
-            $response = $this->httpClient->request('POST', self::SERVICE_B_ENDPOINT, ['json' => $this->payloads[$payload]]);
-        } catch (RequestException $e) {
-            $this->logger->error('Error (' . $e->getCode() . ') while sending payload: ' . $e->getMessage());
-            
-            $response = $e->getResponse();
-        }
+        $response = $this->sendJsonPayload($this->payloads[$payload], self::SERVICE_B_ENDPOINT);
 
         return $this->json(json_decode($response->getBody()), $response->getStatusCode());
     }
@@ -62,24 +57,51 @@ class ServiceController extends AbstractController
     public function runBoth() : JsonResponse 
     {
         $messages = [];
-        foreach ($this->payloads as $key => $payload) {
-            try {
-                $this->logger->info('Sending payload "' . $key . '" to "' . self::SERVICE_B_ENDPOINT .'"');
-
-                // fire http request to ServiceB using the current payload.
-                $response = $this->httpClient->request('POST', self::SERVICE_B_ENDPOINT, ['json' => $payload]);
-            } catch (RequestException $e) {
-                $this->logger->error('Error (' . $e->getCode() . ') while sending payload: ' . $e->getMessage());
-                
-                $response = $e->getResponse();
-            }
-
+        foreach ($this->payloads as $payload) {
+            // send payload to ServiceB
+            $response = $this->sendJsonPayload($payload, self::SERVICE_B_ENDPOINT);
+            
+            // json decode response message and store it for later
             $responseMessage = json_decode($response->getBody(), true);
             $messages[$response->getStatusCode()][] = $responseMessage['data'] ?? $responseMessage;
         }
 
+        // get the higher status code set (the array last key) and return it in the response.
         $maxStatusCode = array_key_last($messages);
 
         return $this->json(['data' => (object) $messages], $maxStatusCode);
+    }
+
+        
+    /**
+     * Send the passed payload in a json http request to the given endpoint.
+     *
+     * @param  array $payload the payload to send
+     * @param  string $endpoint the full url to use for the request 
+     * @param  string $method the request method, 'POST' by default.
+     * 
+     * @return Response
+     */
+    private function sendJsonPayload(array $payload, string $endpoint, string $method = 'POST'): Response
+    {
+        try {
+            $this->logger->info('Sending payload "' . $payload . '" to "' . $endpoint .'"');
+
+            // send json http request to ServiceB using the payload.
+            $response = $this->httpClient->request(
+                $method, $endpoint, 
+                ['json' => $payload],
+                ['headers' => [
+                    'Content-Type' => 'application/json',
+                    'Accept' => 'application/json'
+                ]]
+            );
+        } catch (RequestException $e) {
+            $this->logger->error('Error (' . $e->getCode() . ') while sending payload: ' . $e->getMessage());
+            // get the original response from the exception
+            $response = $e->getResponse();
+        }
+
+        return $response;
     }
 }
